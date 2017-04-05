4a17752c28a6191dcf67bb54c0c986a6ff696420
yabdallah <yanis.abdallah@insa-cvl.fr>
2017-04-05 12:21:55 +0200

    fin td1

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -2,6 +2,9 @@
 package example;
 
 import org.apache.commons.lang.WordUtils;
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+
 
 public class Hello {
     public static void main(String[] args) {
@@ -9,6 +12,9 @@ public class Hello {
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
+    	int monmax=0;
+		CSVReader reader=new CSVReader(new FileReader("data.csv"));
+		list<String[]> myEntries = reader.readAll();
     }
 }
         
