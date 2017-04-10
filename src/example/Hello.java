f82dc22c3f8ab3f38ca31b977143c918b74cf83f
yabdallah <yanis.abdallah@insa-cvl.fr>
2017-04-10 16:38:17 +0200

    ajout de la fonction de max

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -1,20 +1,32 @@
-
 package example;
-
 import org.apache.commons.lang.WordUtils;
+import java.util.List;
 import com.opencsv.CSVReader;
 import java.io.FileReader;
-
-
+import java.io.IOException;
+import java.io.FileNotFoundException;
 public class Hello {
-    public static void main(String[] args) {
-        String  message = "hello ivy !";
-        System.out.println("standard message : " + message);
-        System.out.println("capitalized by " + WordUtils.class.getName() 
-                                + " : " + WordUtils.capitalizeFully(message));
-    	int monmax=0;
-		CSVReader reader=new CSVReader(new FileReader("data.csv"));
-		list<String[]> myEntries = reader.readAll();
-    }
+	public static void main(String[] args) {
+		String  message = "hello ivy !";
+		System.out.println("standard message : " + message);
+		System.out.println("capitalized by " + WordUtils.class.getName() 
+								+ " : " + WordUtils.capitalizeFully(message));
+		Hello app = new Hello();
+		int myMax = 0;
+		String filename ="data.csv";
+		try {
+			CSVReader reader = new CSVReader(new FileReader("data.csv"));
+			List<String[]> myEntries=reader.readAll();
+			for(String[] entry : myEntries){
+				int nb = Integer.parseInt(entry[0]);
+				System.out.println(String.format("Nombre lu : %d", nb));
+				if (nb>myMax)
+					myMax=nb;
+			}
+			System.out.println("le maximum est :"+myMax);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
-        
